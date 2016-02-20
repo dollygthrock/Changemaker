c0ec5a3e182614a2b0150c8bb79abdf622976698
dollygthrock <dollygthrock@gmail.com>
2016-02-20 16:56:55 -0500

    change of 25 with refactor

--- change_maker2.rb
+++ change_maker2.rb
@@ -1,8 +1,13 @@
 def change_maker(cents)
 	total = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+	quarters = 25
 	dimes = 10
 	nickels = 5
 	pennies = 1
+	while cents >= quarters
+		total["quarters"] = total["quarters"] + 1
+		cents = cents - quarters
+	end
 	while cents >= dimes
 		total["dimes"] = total["dimes"] + 1
 		cents = cents - dimes
--- make_change_test2.rb
+++ make_change_test2.rb
@@ -43,6 +43,10 @@ class TestChangeMaker < Minitest::Test
 		assert_equal({"quarters" => 0, "dimes" => 2, "nickels" => 0, "pennies" => 0}, change_maker(20))
 	end
 
+	def test_change_is_25
+		assert_equal({"quarters" => 1, "dimes" => 0, "nickels" => 0, "pennies" => 0}, change_maker(25))
+	end
+
 
 
 
