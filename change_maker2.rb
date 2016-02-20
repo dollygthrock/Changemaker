3e68fe3f41a13b668393866c795d18c4a2260956
dollygthrock <dollygthrock@gmail.com>
2016-02-20 15:52:26 -0500

    Test for 1 and 3 change

--- change_maker2.rb
+++ change_maker2.rb
@@ -1,3 +1,7 @@
 def change_maker(cents)
-	total = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+	total = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => cents}
 end 
+
+
+
+
--- make_change_test2.rb
+++ make_change_test2.rb
@@ -4,8 +4,18 @@ require_relative "change_maker2.rb"
 class TestChangeMaker < Minitest::Test
 
 	def test_change_is_0
-	assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}, change_maker(0))
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}, change_maker(0))
 	end
+
+	def test_change_is_1
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 1}, change_maker(1))
+	end
+
+	def test_change_is_2
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 2}, change_maker(2))
+	end
+
+
 
 end
 
