b214929bfe9f529f4a4de0d2c42d3d3be8663981
dollygthrock <dollygthrock@gmail.com>
2016-02-20 16:04:57 -0500

    test for 5

--- change_maker2.rb
+++ change_maker2.rb
@@ -1,6 +1,17 @@
 def change_maker(cents)
-	total = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => cents}
+	total = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+
+	if cents >= 5
+	total["nickels"] = 1
+	else
+	total["pennies"] = cents
+
+	end
+	total
 end 
+
+
+
 
 
 
--- make_change_test2.rb
+++ make_change_test2.rb
@@ -15,6 +15,10 @@ class TestChangeMaker < Minitest::Test
 		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 2}, change_maker(2))
 	end
 
+	def test_change_is_5
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 1, "pennies" => 0}, change_maker(5))
+	end
+
 
 
 end
