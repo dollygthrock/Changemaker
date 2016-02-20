85bd3fac0090871046735bb917a19c7a61216ef3
dollygthrock <dollygthrock@gmail.com>
2016-02-20 15:21:25 -0500

    first test definite pass

--- make_change_test2.rb
+++ make_change_test2.rb
@@ -2,5 +2,6 @@ require "minitest/autorun"
 require_relative "change_maker.rb"
 
 	def test_change_is_0
-	assert_equal("quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0)
+	assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}, change_maker(0))
 	end
+
